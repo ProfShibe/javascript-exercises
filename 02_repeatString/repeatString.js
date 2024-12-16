9e1386ccfd4fe9d40fbb122bdaf6fddf267a5c62
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-15 20:00:59 -0600

    Finish assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let buffer = "";
+
+  if (num < 0)
+    return "ERROR";
+  else 
+  {
+    for (let i = 0; i < num; i++)
+    {
+      buffer += string;
+    }
+
+    return buffer;
+  }
+  
 
 };
 
