978738e11088b5b613f9e89d43314dcc438380e6
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-15 20:14:38 -0600

    Finish assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let buffer = "";
 
+  for (let i = 1; i < string.length + 1; i++) {
+    buffer += string[string.length - i];
+  }
+
+  return buffer;
 };
 
 // Do not edit below this line
