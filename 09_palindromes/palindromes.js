946506f8fae72527b91eca20ab2b1480e9ef41e1
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-28 19:41:59 -0600

    Finish assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
+const palindromes = (str) => {
+  cleaned_str = str.split(/[^a-zA-Z0-9]/).join('').toLowerCase();
+  return cleaned_str === cleaned_str.split('').reverse().join('');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
