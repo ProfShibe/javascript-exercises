5e45f76964bc3f6770a75437e08a2c0d2caeaec2
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-29 19:49:11 -0600

    Finish assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(iterations) {
+  if (iterations < 0)
+    return "OOPS";
+  else if (iterations == 0) {
+    return 0;
+  }
+  else {
+    let counter = 0;
+    let start = 0;
+    let end = 1;
+    let sum = 0;
 
+    do
+    {
+        sum = start + end;
+        start = end;
+        end = sum;
+        counter++;
+    } 
+    while (counter != iterations)
+
+    return start;
+  }
 };
 
 // Do not edit below this line
