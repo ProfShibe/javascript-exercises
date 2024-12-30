fe6eadbe87669aa2053f683c7838c69d29f67980
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-29 20:35:41 -0600

    Finish assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  let this_year = new Date().getFullYear();
+  let oldest_age = 0;
+  let oldest_person = "";
 
+  arr.forEach(element => {
+    if (typeof element.yearOfDeath === "undefined") {
+        age = this_year - element.yearOfBirth;
+    } 
+    else {
+        age = element.yearOfDeath - element.yearOfBirth;
+    }
+
+    if (age > oldest_age) {
+      oldest_age = age;
+      oldest_person = element;
+    }
+  });
+
+  return oldest_person;
 };
 
 // Do not edit below this line
